4f769b0c1d7fef6ce4ddae83fa4c431f869d9c63
Mohammad Azam <azamsharp@gmail.com>
2021-05-29 14:56:24 -0500

    movies graphql server

---
+++ app.js
@@ -0,0 +1,80 @@
+
+
+let movies = require('./data/movies')
+const genres = require('./data/genres')
+
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+
+    type Movie {
+        id: ID! 
+        title: String! 
+        year: String! 
+        genre: String! 
+        poster: String! 
+    }
+
+    type Genre {
+        name: String! 
+    }
+
+    input MovieInput {
+        id: ID
+        title: String! 
+        year: String! 
+        genre: String! 
+        poster: String! 
+    }
+
+    input MovieFilter {
+        genre: String 
+    }
+
+    type Query {
+        movies(filter: MovieFilter): [Movie]
+        genres: [Genre]
+    }
+
+    type Mutation {
+        addMovie(movie: MovieInput): Movie 
+    }
+`
+
+const getAllMovies = (filter) => {
+
+    let filteredMovies = movies 
+
+    if(filter) {
+        if(filter.genre) {
+            filteredMovies = movies.filter(movie => movie.genre == filter.genre)
+        }
+    }
+
+    console.log(filteredMovies)
+    return filteredMovies
+
+}
+
+const addMovie = (movie) => {
+    movie.id = movies.length + 1 
+    movies.push(movie)
+    return movie 
+}
+
+const resolvers = {
+    Query: {
+        movies: (_, { filter }) => getAllMovies(filter), 
+        genres: () => genres  
+    }, 
+    Mutation: {
+        addMovie: (_, { movie }) => addMovie(movie)
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+const PORT = process.env.PORT || 8080 
+
+server.listen(PORT).then(({url}) => {
+    console.log(`Server is running at ${url}`)
+})
